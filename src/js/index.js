8cd336e1b8b3bc404b26e250e7aca5866b67204a
siraaj <siraajssandhu@gmail.com>
2024-02-23 23:42:12 -0700

    1st ver

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
 const content = document.querySelector('.content');
-const scrollIndicator = document.querySelector('.scroll-indicator');
+const scrollIndicator = document.querySelector('.scroll-indicator');  
+const cards = [...content.children]
+  .filter(c => c.classList.contains('card'));
 
 window.requestAnimationFrame(function updateScrollAccessibility() {
   const overflow = content.scrollHeight - content.clientHeight > 0;
-  const canScroll = Math.floor(content.scrollTop) < (content.scrollHeight - content.clientHeight);
+  const canScroll = content.scrollTop < 0.95 * (content.scrollHeight - content.clientHeight);
+
+  // console.log(Math.floor)
 
   if (canScroll) {
     scrollIndicator.classList.remove('hidden');
@@ -11,5 +15,21 @@ window.requestAnimationFrame(function updateScrollAccessibility() {
     scrollIndicator.classList.add('hidden');
   }
 
+  const previewCards = [];
+
+  cards.forEach(card => {
+    const rect = card.getBoundingClientRect();
+    if (rect.top > 0.9 * window.innerHeight) {
+      previewCards.push(card.children[0].textContent);
+    }
+  });
+
+  const thresh = 30;
+  let previewText = previewCards.join(', ');
+  if (previewText.length > thresh) {
+    previewText = previewText.substring(0, thresh) + '&#x2026;';
+  }
+
+  scrollIndicator.innerHTML = '&#x2193; ' + previewText;
   window.requestAnimationFrame(updateScrollAccessibility);
 });
